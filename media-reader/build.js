868edad524287b724dfbe6d20fa6f938c02dfc89
user-9902 <xy_frontend@163.com>
2025-10-20 21:52:33 +0800

    feat: media reader

--- media-reader/build.js
+++ media-reader/build.js
@@ -1,38 +1,38 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
 // 文件路径
-const sourcePath = path.join(__dirname, "src/index.js");
-const targetPath = path.join(__dirname, "dist.js");
+const sourcePath = path.join(__dirname, 'src/index.js')
+const targetPath = path.join(__dirname, 'dist.js')
 
 // 读取public文件夹结构
-const publicDir = path.join(__dirname, "public");
+const publicDir = path.join(__dirname, 'public')
 function readDirRecursive(dir) {
-  const res = { name: path.basename(dir), children: [], files: [] };
+  const res = { name: path.basename(dir), children: [], files: [] }
 
-  const list = fs.readdirSync(dir);
+  const list = fs.readdirSync(dir)
 
-  list.forEach((file) => {
-    const filePath = path.join(dir, file);
-    const stat = fs.statSync(filePath);
+  list.forEach(file => {
+    const filePath = path.join(dir, file)
+    const stat = fs.statSync(filePath)
     if (stat && stat.isDirectory()) {
-      res.children.push(readDirRecursive(filePath));
+      res.children.push(readDirRecursive(filePath))
     } else {
-      res.files.push(path.relative(__dirname, filePath));
+      res.files.push(path.relative(__dirname, filePath).replace(/\\/g, '/'))
     }
-  });
-  return res;
+  })
+  return res
 }
 
-const menus = readDirRecursive(publicDir);
+const menus = readDirRecursive(publicDir)
 
-fs.readFile(path.join(__dirname, "template.html"), "utf8", (err, data) => {
+fs.readFile(path.join(__dirname, 'template.html'), 'utf8', (err, data) => {
   // 使用replace方法替换目标模板
   const newData = data.replace(
     /{{\s*template\s*}}/g,
-    JSON.stringify(menus, null, 2)
-  );
+    JSON.stringify(menus, null, 2),
+  )
 
   // 将更新后的内容写回文件
-  fs.writeFile(path.join(__dirname, "dist.html"), newData, (writeErr) => {});
-});
+  fs.writeFile(path.join(__dirname, 'dist.html'), newData, writeErr => {})
+})
